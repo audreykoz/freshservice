0e6a34b01f179501253fc24e25ac55e161c8fc5f
VladislavEkimtcov <ekimtcov.v@gmail.com>
2019-09-26 19:44:31 -0500

    NEW, UNTESTED: main.py: * wrapper that makes it possible to operate the freshservice tool from the command line

---
+++ main.py
@@ -0,0 +1,45 @@
+import argparse
+import csv2cmdb as c
+import re
+
+
+def ingest(args):
+    """Upload the elements and connections to Freshservice"""
+    c.add_update_assets(args.elemfile, re.match(args.elemfile, '[^.]*$'))
+    c.add_rela(args.relfile, args.elemfile)
+
+
+def delete(args):
+    """Delete the elements and connections from Freshservice"""
+    # delete_asset ?
+    pass
+
+
+if __name__ == "__main__":
+
+    main_parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    main_parser.add_argument("--elemfile", "-e", default=None, help='element file')
+    main_parser.add_argument("--relfile", "-r", default=None, help='relationships file')
+
+    subparsers = main_parser.add_subparsers(title="subcommands",
+                                            description='valid subcommands',
+                                            help='additional help')
+
+    ingest_parser = subparsers.add_parser('ingest', description=ingest.__doc__)
+    ingest_parser.set_defaults(func=ingest)
+
+    delete_parser = subparsers.add_parser('delete', description=delete.__doc__)
+    delete_parser.set_defaults(func=delete)
+
+    args = main_parser.parse_args()
+
+    if not args.elemfile or not args.relfile:
+        print('Please provide both the elements and relationships file')
+        exit(1)
+
+    if not args.func:  # there are no subfunctions
+        main_parser.print_help()
+        exit(1)
+    args.func(args)
